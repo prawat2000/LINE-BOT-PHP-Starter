8f5939fac9fd24d4aaf3ea9fb2ebb3c65e94f0ba
prawat2000 <prawat.th@gmail.com>
2016-10-14 15:35:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,13 +9,13 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		if($event['type'] == 'sticker'){
+		
 		  $sticker = [
 		  	'type' => 'sticker',
 			'packageID' => '1',
 			'stickerID' => '1'  
 		  ];	
-		}
+		
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
